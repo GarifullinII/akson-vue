547826ad0a4f34bf5a02934222440042cef292ed
GarifullinII <ildaridrisovich@gmail.com>
2021-10-15 21:15:35 +0400

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,40 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </div>
-  <router-view/>
+<div>
+	<div class="post">
+		<div><strong>Name:</strong>About JavaScript</div>
+		<div><strong>Description:</strong>JavaScript is it best</div>
+	</div>
+</div>
 </template>
 
+<script>
+export default {
+	data() {
+		return {
+			likes: 0,
+			dislikes: 0,
+		}
+	},
+	methods: {
+		addLike() {
+			this.likes += 1;
+		},
+		addDislike() {
+			this.dislikes +=1;
+		}
+	}
+}
+</script>
+
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+* {
+	margin: 0;
+	padding: 0;
+	box-sizing: border-box;
 }
 
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
+.post {
+	padding: 15px;
+	border: 2px solid teal;
 }
 </style>
